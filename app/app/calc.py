0f3ad5cba31184ca23fefd53873181c6c29b2f5e
Alain Patrick dela Pena <alaindee@Alains-MacBook-Pro.local>
2023-02-27 23:50:42 +1100

    Configured docker compose and check to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """ addx and y and return results"""
+    return x + y
+
+
+def subtract(y, x):
+    """Subtract x from y and return result."""
+    return y - x
